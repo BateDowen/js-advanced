9d8a74eef8f57590f2439e16576bcbebb4c39eb1
BateDowen <miroslav.dimitrov1994@gmail.com>
2022-10-13 11:20:12 +0300

    Update test.js

--- Syntax, Functions and Statements - Lab/test.js
+++ Syntax, Functions and Statements - Lab/test.js
@@ -1,9 +1,23 @@
 function solve(params) {
-let obj = {};
-obj.name = 'pesho';
-console.log(obj);
-obj = {...obj, age: 30, city: 'Plovdiv'};
+    
 
-console.log(obj.age);
+let obj = {};
+obj.name = 'Pesho';
+console.log(obj);
+obj = {
+    ...obj,
+     age: 30, 
+     city: 'Plovdiv',
+     run : function() {console.log(`${this.name} Run`)} 
+    };
+obj.run()
+obj.name = 'Miro'
+obj.run()
+
+let stop = {
+    name: 'Kras'
+}
+stop.stopRun = obj.run;
+stop.stopRun()
 }
 solve()
