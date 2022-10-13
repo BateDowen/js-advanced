50bb4527a1556877ab58fd491175262d87457e23
BateDowen <miroslav.dimitrov1994@gmail.com>
2022-10-13 11:19:57 +0300

    Update 03. City Taxes.js

--- Objects and Composition - Lab/03. City Taxes.js
+++ Objects and Composition - Lab/03. City Taxes.js
@@ -1,5 +1,24 @@
-function solve(input) {
- 
+function cityTaxes(name, population, treasury) {
+
+    let obj = {
+        name : name,
+        population: population,
+        treasury : treasury,
+        taxRate : 10,
+        collectTaxes() {
+            this.treasury += this.population*this.taxRate
+        },
+        applyGrowth(percentage) {
+            this.population += this.population * percentage / 100;
+
+        }, 
+        applyRecession(percentage) {
+            this.treasury -= this.treasury * percentage /100;
+
+        }
+    }
+    return obj;
+
 }
 const city =
   cityTaxes('Tortuga',
@@ -9,4 +28,3 @@ city.collectTaxes();
 console.log(city.treasury);
 city.applyGrowth(5);
 console.log(city.population);
-solve()
