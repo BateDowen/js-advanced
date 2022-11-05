b563f57376b7f48de65080b4290ba9385eb6b608
BateDowen <miroslav.dimitrov1994@gmail.com>
2022-11-05 08:54:03 +0200

    Update solution.js

--- DOM Introduction - Lab/Dom - Exercise/01. Subtraction_Ресурси/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ DOM Introduction - Lab/Dom - Exercise/01. Subtraction_Ресурси/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,38 +1,3 @@
 function solve() {
-  let input = document.getElementById('text').value;
-  let currentCase = document.getElementById('naming-convention').value;
-  let result = '';
-  
-  if (currentCase === "Camel Case") {
-
-    for(let i =0; i < input.length; i++){
-      if (input[i] === ' ') {
-        result += input[i+1].toUpperCase();
-        i++;
-
-      }else{
-        result += input[i].toLowerCase()
-      }
-    
-    }
-
-  }else if (currentCase === "Pascal Case") {
-    result += input[0].toUpperCase();
-
-    for(let i = 1; i < input.length; i++){
-      if (input[i] === ' ') {
-        result += (input[i+1].toUpperCase());
-        i++;
-
-      }else{
-        result += input[i].toLowerCase();
-
-      }
-    }
-  }else{
-    result = "Error!"
-  }
-  let resElement = document.getElementById('result');
-  resElement.textContent = result;
-
+  //TODO...
 }
