b038199c7a145bc8453fde7bacdeea93916c7d8f
BateDowen <miroslav.dimitrov1994@gmail.com>
2022-11-08 14:11:44 +0200

    Update app.js

--- DOM-Manipulations/DOM-Manipulations-Exercise/03. Locked-Profile/app.js
+++ DOM-Manipulations/DOM-Manipulations-Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,26 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let showMoreBtn = document.getElementsByTagName('button');
+
+        Array.from(showMoreBtn).forEach((el) => {
+            el.addEventListener('click', (e) =>{
+                let hiddenDiv = e.target.previousElementSibling;
+                let text = e.currentTarget.textContent;
+                let inputUnlockEl = e.currentTarget.parentNode.children[4];
+                if (inputUnlockEl.checked) {
+                    
+                    if (text === 'Show more'){
+                    hiddenDiv.style.display = 'inline-block';
+                    el.textContent = 'Hide it';
+                        
+                    } else if(text === 'Hide it') {
+                        hiddenDiv.style.display = 'none';
+                        el.textContent = 'Show more';
+    
+                    }
+                }
+            })
+                
+        })
+        
+    
 }
