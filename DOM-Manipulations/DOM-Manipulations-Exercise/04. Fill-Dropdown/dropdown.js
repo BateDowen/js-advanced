29b4b18bdf1fb0871ea1c061aa6a2c50fec52e12
BateDowen <miroslav.dimitrov1994@gmail.com>
2022-11-08 14:11:51 +0200

    Update dropdown.js

--- DOM-Manipulations/DOM-Manipulations-Exercise/04. Fill-Dropdown/dropdown.js
+++ DOM-Manipulations/DOM-Manipulations-Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+    let addButton = document.querySelector('input[value="Add"]');
+    addButton.addEventListener('click', add);
+    function add(){
+    let menuItem = document.getElementById('menu')
+    let textItem = document.getElementById('newItemText');
+    let valueItem = document.getElementById('newItemValue');
+    let optionEl = document.createElement('option');
+    optionEl.value = valueItem.value
+    optionEl.textContent = textItem.value;
+    menuItem.appendChild(optionEl);
+    textItem.value = '';
+    valueItem .value= '';
+    }
+
 }
