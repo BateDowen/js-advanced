5f5442a99caa2c6241be110ea5828fedc69b33fa
BateDowen <miroslav.dimitrov1994@gmail.com>
2022-11-21 09:32:48 +0200

    Update solution.js

--- DOM-Manipulations/DOM-Manipulations-Exercise/08. Sudomu/solution.js
+++ DOM-Manipulations/DOM-Manipulations-Exercise/08. Sudomu/solution.js
@@ -1,4 +1,15 @@
 function solve() {
+    let [quickCheckBtn, clearBtn] = document.querySelectorAll('button');
 
-    //TODO...
+    quickCheckBtn.addEventListener('click', quickCheck);
+
+    function quickCheck() {
+        let firtsChildArr = [];
+
+        let trElements = document.querySelectorAll('tbody tr')
+        trElements.forEach(el=>{
+            firtsChildArr.push(el.firstElementChild.firstElementChild);
+        });
+       
+    }
 }
