a8e3c89bacd1c4accdb42696a182bcbcf03debc0
BateDowen <miroslav.dimitrov1994@gmail.com>
2022-12-02 09:58:25 +0200

    Create 01. Sub Sum.js

---
+++ Unit Testing and Error Handling - Lab/01. Sub Sum.js
@@ -0,0 +1,11 @@
+function solve(numbers,start,end) {
+    if (!Array.isArray(numbers)) {
+       return NaN; 
+    }
+    let startIndex = Math.max(start,0);
+    let endIndex = Math.min(end,numbers.length - 1);
+    let sumNumbers = numbers.slice(startIndex,endIndex + 1);
+    let sum = sumNumbers.reduce((a,x) => a + Number(x),0);
+    return sum
+}
+console.log(solve([10, 20, 30, 40, 50, 60], 3, 300));
